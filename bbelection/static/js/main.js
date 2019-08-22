4037d77455e8108a10820f117f25623017bd5879
cudjoeab <cudjoeab@gmail.com>
2019-08-22 15:31:15 -0400

    added button that leads to the list of all the details

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    let rootButton = document.getElementById('request-root');
+    let candidateInfo = document.getElementById('candidate-info');
+
+    rootButton.addEventListener('click', () => {
+        let request = 
+        axios.get('https://eric-deploy-test.herokuapp.com/bb')
+             .then(function(response){
+                 const currentCandidates = response.data.candidates
+                 console.log('--test successful--') 
+                 console.log(currentCandidates)
+                 currentCandidates.forEach(element => {
+                    const candidate = document.createElement('li')
+                    candidate.innerHTML = `${element.name} got ${element.votes} votes.`
+                    candidateInfo.appendChild(candidate)
+                 });
+                 
+             })
+            
+                
+
+             
+             
+    });
+
+
 });
