739757dba2d1970fafe9d35dffe30f7e9b974c27
cudjoeab <cudjoeab@gmail.com>
2019-08-26 16:46:19 -0400

    user can vote using AJAX request

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let rootButton = document.getElementById('request-root');
     let candidateInfo = document.getElementById('candidate-info');
 
-
-    let request = 
+    rootButton.addEventListener('click', function() {
+        let request = 
         axios.get('https://bb-election-api.herokuapp.com')
              .then(function(response){
                  const currentCandidates = response.data.candidates
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
                     const voteForm = document.createElement('form')
                     voteForm.method = 'POST'
-                    voteForm.action= "https://bb-election-api.herokuapp.com/vote"
+                    voteForm.action= `https://bb-election-api.herokuapp.com/vote?id=${element.id}`
                     voteForm.setAttribute('name', 'id')
                     
                     const voteButton = document.createElement('input')
@@ -26,9 +26,30 @@ document.addEventListener("DOMContentLoaded", function() {
 
                     voteForm.appendChild(voteButton)    
                     candidate.appendChild(voteForm) 
+
+                    voteButton.addEventListener('click',(e) => {
+                        e.preventDefault()
+                        let vote = axios.post(`https://bb-election-api.herokuapp.com/vote?id=${element.id}`)
+                        .then(function(response) {
+                             console.log('--user voted!')
+                        })
+                        .catch(function(error){
+                            console.log('error')
+                        })
+                        .then(function(response){
+                            console.log('voting ended')
+                        }); 
+                        
+                    });
                  });
                  
              }); 
+    
+        
+
+    }); 
+    
+    
 
     
 
