93f299ba26b76d460e6d14e027c035770b2f353c
cudjoeab <cudjoeab@gmail.com>
2019-08-26 15:26:26 -0400

    added vote button to each candidate

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
     let rootButton = document.getElementById('request-root');
     let candidateInfo = document.getElementById('candidate-info');
 
-    rootButton.addEventListener('click', () => {
-        let request = 
-        axios.get('https://eric-deploy-test.herokuapp.com/bb')
+
+    let request = 
+        axios.get('https://bb-election-api.herokuapp.com')
              .then(function(response){
                  const currentCandidates = response.data.candidates
                  console.log('--test successful--') 
@@ -14,15 +14,23 @@ document.addEventListener("DOMContentLoaded", function() {
                     const candidate = document.createElement('li')
                     candidate.innerHTML = `${element.name} got ${element.votes} votes.`
                     candidateInfo.appendChild(candidate)
+
+                    const voteForm = document.createElement('form')
+                    voteForm.method = 'POST'
+                    voteForm.action= "https://bb-election-api.herokuapp.com/vote"
+                    voteForm.setAttribute('name', 'id')
+                    
+                    const voteButton = document.createElement('input')
+                    voteButton.type ='submit'
+                    voteButton.value = 'VOTE'
+
+                    voteForm.appendChild(voteButton)    
+                    candidate.appendChild(voteForm) 
                  });
                  
-             })
-            
-                
+             }); 
 
-             
-             
-    });
+    
 
 
 });
